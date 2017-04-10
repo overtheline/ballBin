f5d7c5943462117a2159b864631a25422ec58b4f
aaronCKeel <aaronckeel@gmail.com>
2017-04-09 18:04:10 -0700

    change state on collision

--- collide.js
+++ collide.js
@@ -5,8 +5,8 @@ function collide(obj1, obj2) {
   const colnVec = new Vector(x2 - x1, y2 - y1);
   const dist = colnVec.length();
 
-  const newState1 = {};
-  const newState2 = {};
+  const newState1 = { fill: 'blue' };
+  const newState2 = { fill: 'blue' };
 
   if (dist < r1 + r2) {
     colnVec.normalize();
@@ -20,17 +20,22 @@ function collide(obj1, obj2) {
   const cy1 = newState1.y;
 
   const colnAngle = Math.atan2(cy1 - y2, cx1 - x2);
+
   const vec1 = new Vector(dx1, dy1);
   const vec2 = new Vector(dx2, dy2);
+
   const len1 = vec1.length();
   const len2 = vec2.length();
+
   const dirAngle1 = Math.atan2(vec1.vy, vec1.vx);
   const dirAngle2 = Math.atan2(vec2.vy, vec2.vx);
 
   vec1.vx = len2 * Math.cos(dirAngle2 - colnAngle);
+  vec1.vy = len2 * Math.sin(dirAngle2 - colnAngle);
+
   vec2.vx = len1 * Math.cos(dirAngle1 - colnAngle);
-  vec1.vy = len1 * Math.sin(dirAngle1 - colnAngle);
-  vec2.vy = len2 * Math.sin(dirAngle2 - colnAngle);
+  vec2.vy = len1 * Math.sin(dirAngle1 - colnAngle);
+
   vec1.rotate(colnAngle);
   vec2.rotate(colnAngle);
 
